b59302d4aa035cce3b572a67060050ca6f4ddde2
AshimBarman <ashlabbarman@gmail.com>
2023-04-16 19:55:43 +0530

    Added get user by id in repository

--- src/index.js
+++ src/index.js
@@ -7,6 +7,7 @@ const apiRoutes = require('./routes/index');
 
 // const { User } = require('./models/index');
 // const bcrypt = require('bcrypt');
+const UserRepository = require('./repository/user-repository');
 
 
 const app = express();
@@ -20,6 +21,9 @@ const prepareAndStartServer = () => {
 
     app.listen(PORT, async () => {
         console.log(`server Started at port ${PORT}`);
+        const repo = new UserRepository();
+        const response = await repo.getById(5);
+        console.log(response);
         // const incommingpassword = '1234amakusidediore@';
         // const user = await User.findByPk(5);
         // const response = bcrypt.compareSync(incommingpassword, user.password);
